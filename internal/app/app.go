1827d2b03cef96a9d2303e8852b0b4d73fef12fa
grokkos <gerokkos@gmail.com>
2023-01-29 02:43:21 +0200

    profit and loss calculations - price entity into pkg

--- internal/app/app.go
+++ internal/app/app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/Finnhub-Stock-API/finnhub-go/v2"
+	"github.com/grokkos/finnio/internal/pkg/entities"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
@@ -15,14 +16,27 @@ func App() {
 	if err != nil {
 		log.Fatalf("Error getting env, %v", err)
 	} else {
-		fmt.Println("We are getting the env values")
+		fmt.Println(".env file loaded with success")
 	}
 	cfg := finnhub.NewConfiguration()
 	cfg.AddDefaultHeader("X-Finnhub-Token", os.Getenv("API_KEY"))
 	finnhubClient := finnhub.NewAPIClient(cfg).DefaultApi
 
 	res, _, _ := finnhubClient.Quote(context.Background()).Symbol("AAPL").Execute()
-	//TODO lets move logic and calculations to app and play there
 	//TODO begin unit testing
-	fmt.Printf("%+v\n", *res.Pc)
+	t := &entities.Price{Current: *res.C, Previous: *res.Pc}
+
+	//check for profit and fill the response struct object accordingly
+	if *res.C > *res.Pc {
+		t.Profit = *res.C - *res.Pc
+	} else if *res.C < *res.Pc {
+		t.Loss = *res.Pc - *res.C
+	}
+
+	fmt.Printf("%+v\n", t)
+
+	//previousClosing := *res.Pc MSFT
+
+	//goroutine to check the MSFT price parallel
+	//res1, _, _ := finnhubClient.Quote(context.Background()).Symbol("MSFT").Execute()
 }
