2cd927e4e7f32b2e082fe5501e7024f3892b08ba
grokkos <gerokkos@gmail.com>
2023-01-30 00:08:29 +0200

    calculator func implementation

--- internal/app/app.go
+++ internal/app/app.go
@@ -16,27 +16,33 @@ func App() {
 	if err != nil {
 		log.Fatalf("Error getting env, %v", err)
 	} else {
-		fmt.Println(".env file loaded with success")
+		//fmt.Println(".env file loaded with success")
 	}
 	cfg := finnhub.NewConfiguration()
 	cfg.AddDefaultHeader("X-Finnhub-Token", os.Getenv("API_KEY"))
 	finnhubClient := finnhub.NewAPIClient(cfg).DefaultApi
 
-	res, _, _ := finnhubClient.Quote(context.Background()).Symbol("AAPL").Execute()
-	//TODO begin unit testing
-	t := &entities.Price{Current: *res.C, Previous: *res.Pc}
+	appleData, _, _ := finnhubClient.Quote(context.Background()).Symbol("AAPL").Execute()
 
-	//check for profit and fill the response struct object accordingly
-	if *res.C > *res.Pc {
-		t.Profit = *res.C - *res.Pc
-	} else if *res.C < *res.Pc {
-		t.Loss = *res.Pc - *res.C
+	entityApple := &entities.Price{Current: *appleData.C, Previous: *appleData.Pc}
+
+	//use the calcProfitAndLoss function to do the maths
+	entityApple.Loss, entityApple.Profit = calcProfitAndLoss(*appleData.C, *appleData.Pc)
+	fmt.Printf("%+v\n", entityApple)
+
+	MicrosoftData, _, _ := finnhubClient.Quote(context.Background()).Symbol("MSFT").Execute()
+	entityMicrosoft := &entities.Price{Current: *MicrosoftData.C, Previous: *MicrosoftData.Pc}
+
+	//use the calcProfitAndLoss function to do the maths
+	entityMicrosoft.Loss, entityMicrosoft.Profit = calcProfitAndLoss(*MicrosoftData.C, *MicrosoftData.Pc)
+	fmt.Printf("%+v\n", entityMicrosoft)
+}
+
+func calcProfitAndLoss(current float32, previousClosing float32) (loss float32, profit float32) {
+	if current > previousClosing {
+		profit = current - previousClosing
+	} else if current < previousClosing {
+		loss = previousClosing - current
 	}
-
-	fmt.Printf("%+v\n", t)
-
-	//previousClosing := *res.Pc MSFT
-
-	//goroutine to check the MSFT price parallel
-	//res1, _, _ := finnhubClient.Quote(context.Background()).Symbol("MSFT").Execute()
+	return
 }
