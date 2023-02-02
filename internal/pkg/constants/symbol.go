6e58b0a7be4f46ae86a4666dcb8854e4286e5429
grokkos <gerokkos@gmail.com>
2023-02-02 18:24:00 +0200

    comment on enum for symbols

--- internal/pkg/constants/symbol.go
+++ internal/pkg/constants/symbol.go
@@ -5,4 +5,5 @@ type ShareSymbol string
 const (
 	ShareSymbolApple     ShareSymbol = "AAPL"
 	ShareSymbolMicrosoft ShareSymbol = "MSFT"
+	//here will go the rest of the shares symbols in order to support
 )
